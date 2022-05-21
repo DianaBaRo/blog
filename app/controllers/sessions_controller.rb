85a3847fb34ee7d5cb04a0f91a6af0705599ad17
Diana Badas <dianabadas@192.168.1.101>
2022-05-21 16:24:04 +0100

    Add login and sign up to app

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+    if current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def create
+    @user = User.find_by(:email => params[:user][:email])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Login info was incorrect. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    if current_user
+      session.delete :user_id
+
+      redirect_to root_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'static_pages#home'
+
+  resources :users
+  
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
